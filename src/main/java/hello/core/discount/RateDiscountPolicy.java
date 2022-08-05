e1c2087417dc69c3f9c7ad64baf8a1ab01945b6b
Jaewoo Kim <wodndhksk@gmail.com>
2022-08-05 20:18:55 +0900

    의존관계 자동주입 - 어노테이션 직접 만들어 사용

--- src/main/java/hello/core/discount/RateDiscountPolicy.java
+++ src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -1,12 +1,14 @@
 package hello.core.discount;
 
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
-@Primary // 조회된 빈이 2개 이상일때 우선순위
+//@Primary // 조회된 빈이 2개 이상일때 우선순위
+@MainDiscountPolicy //만든 빈 사용
 public class RateDiscountPolicy implements DiscountPolicy{
 
     private int discountPercent = 10;
