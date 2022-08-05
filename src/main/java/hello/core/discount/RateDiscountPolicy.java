e6a77474fc9ef0be9a6c5c4d9ac6645c93095156
Jaewoo Kim <wodndhksk@gmail.com>
2022-08-05 20:12:02 +0900

    의존관계 자동주입 - 조회된 빈이 2개 이상일때 @Primary 로 우선순위 설정

--- src/main/java/hello/core/discount/RateDiscountPolicy.java
+++ src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -2,9 +2,11 @@ package hello.core.discount;
 
 import hello.core.member.Grade;
 import hello.core.member.Member;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
+@Primary // 조회된 빈이 2개 이상일때 우선순위
 public class RateDiscountPolicy implements DiscountPolicy{
 
     private int discountPercent = 10;
